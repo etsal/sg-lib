31914556df56fedae8c069face91224952b233c5
Stefanie Dukovac <smdukovac@uwaterloo.ca>
2021-05-22 21:04:54 -0400

    removed CMakeFiles

--- CMakeFiles/Edger8rHeader.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Edger8rHeader"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Edger8rHeader.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
